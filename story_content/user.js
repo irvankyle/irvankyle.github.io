a71f1d272cf02520e2f036f71bb07261f21d61af
Kyle Irvan <69685181+L-Chiki@users.noreply.github.com>
2021-08-27 15:34:07 -0400

    My first commit!

---
+++ story_content/user.js
@@ -0,0 +1,97 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5b83IvsKwMV":
+        Script1();
+        break;
+      case "69hGTevPF1W":
+        Script2();
+        break;
+      case "6AyruSaVq4G":
+        Script3();
+        break;
+      case "5cOKmm7ZlNG":
+        Script4();
+        break;
+      case "5kpMd2qwMH2":
+        Script5();
+        break;
+      case "5dhXcUTEJHM":
+        Script6();
+        break;
+  }
+}
+
+function Script1()
+{
+  var player= GetPlayer();
+(function(){
+   var v = document.getElementsByTagName('video')[0];
+   v.addEventListener('timeupdate',function(event){
+      var playHeadPosition = v.currentTime;
+      player.SetVar("playHead",playHeadPosition);
+   },false);
+})();
+}
+
+function Script2()
+{
+  var player= GetPlayer();
+(function(){
+   var v = document.getElementsByTagName('video')[0];
+   v.addEventListener('timeupdate',function(event){
+      var playHeadPosition = v.currentTime;
+      player.SetVar("playHead",playHeadPosition);
+   },false);
+})();
+}
+
+function Script3()
+{
+  var player= GetPlayer();
+(function(){
+   var v = document.getElementsByTagName('video')[0];
+   v.addEventListener('timeupdate',function(event){
+      var playHeadPosition = v.currentTime;
+      player.SetVar("playHead",playHeadPosition);
+   },false);
+})();
+}
+
+function Script4()
+{
+  var player= GetPlayer();
+(function(){
+   var v = document.getElementsByTagName('video')[0];
+   v.addEventListener('timeupdate',function(event){
+      var playHeadPosition = v.currentTime;
+      player.SetVar("playHead",playHeadPosition);
+   },false);
+})();
+}
+
+function Script5()
+{
+  var player= GetPlayer();
+(function(){
+   var v = document.getElementsByTagName('video')[0];
+   v.addEventListener('timeupdate',function(event){
+      var playHeadPosition = v.currentTime;
+      player.SetVar("playHead",playHeadPosition);
+   },false);
+})();
+}
+
+function Script6()
+{
+  var player= GetPlayer();
+(function(){
+   var v = document.getElementsByTagName('video')[0];
+   v.addEventListener('timeupdate',function(event){
+      var playHeadPosition = v.currentTime;
+      player.SetVar("playHead",playHeadPosition);
+   },false);
+})();
+}
+
